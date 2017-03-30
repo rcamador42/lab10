89a144536f8d82722ca07fe96bb2862fc21c063b
Rayvin Carl Amador <rcamador42@students.tntech.edu>
2017-03-30 07:18:54 -0600

    ADDING ALL FILES TO GITHUB

---
+++ Hybrid.h
@@ -0,0 +1,58 @@
+#if !defined (HYBRID_H)
+#define HYBRID_H
+
+#include "QueueLinked.h"
+using CSC2110::QueueLinked;
+#include "SortedListDoublyLinked.h"
+
+template < class T >
+class Hybrid
+{
+
+   private:
+      //---------------------------------------------
+      // ORIGINALLY QueueLinked<T>* q; 
+      //---------------------------------------------
+      QueueLinked< DoubleNode<T> >* q; 
+      SortedListDoublyLinked<T>* sldl;
+
+   public:
+      Hybrid(int (*comp_items) (T* item_1, T* item_2), int (*comp_keys) (String* key, T* item));
+      ~Hybrid();
+
+      bool isEmpty();
+      void enqueue(T* item);
+      T* dequeue();
+      ListDoublyLinkedIterator<T>* iterator();
+
+};
+
+template < class T >
+Hybrid<T>::Hybrid(int (*comp_items) (T* item_1, T* item_2), int (*comp_keys) (String* key, T* item))
+{
+   //---------------------------------------------
+   // ORIGINALLY q = new QueueLinked<T>();
+   //---------------------------------------------
+   q = new QueueLinked< DoubleNode<T> >();
+   sldl = new SortedListDoublyLinked<T>(comp_items, comp_keys);
+}
+
+template < class T >
+Hybrid<T>::~Hybrid()
+{
+   delete q;
+   delete sldl;
+}
+
+//DO THIS
+//complete the implementation for the Hybrid ADT in two different ways
+//as outlined in the Lab 10 description
+//simply comment the first implementation out when working on the second implementation
+//use the getKey method to dequeue/remove
+
+
+
+
+
+
+#endif
---
+++ Makefile
@@ -0,0 +1,28 @@
+AutomatedMakefile = am
+CC = g++
+CXXFLAGS = -Wno-deprecated-declarations -g -O0
+
+PROJECT_PATH = $(CURRENT_DIR)
+GUI_PATH = $(DRIVE_LETTER)/TDM-GCC-64/wxWidgets-3.1.0
+
+INC_DIRS = -I./2111/CSC2110 -I./2111/GUI -I$(GUI_PATH)/include -I$(GUI_PATH)/lib/gcc510TDM_x64_dll/mswu
+LIB_DIRS = -L./2111/CSC2110 -L./2111/GUI -L$(GUI_PATH)/lib/gcc510TDM_x64_dll
+LDFLAGS = -Wl,-Bstatic
+LIBS = $(LDFLAGS) -lCSC2110 -lrandom -lGUI -lwxmsw31u_core -lwxbase31u -lgdi32
+
+COMPILE = $(CC) $(CXXFLAGS) $(INC_DIRS) -c 
+LINK = $(CC) $(CXXFLAGS) $(LIB_DIRS)
+
+FILES = HybridDriver.o
+EXECUTABLE = Hybrid.exe
+
+all: Hybrid
+
+Hybrid: 	$(FILES)
+			$(LINK) $(FILES) $(LIBS) -o $(EXECUTABLE)
+			$(EXECUTABLE)
+
+HybridDriver.o:
+			$(COMPILE) HybridDriver.cpp
+
+
